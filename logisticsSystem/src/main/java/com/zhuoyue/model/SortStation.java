d208f86987e83915818cc71534782ae973642507
Administrator <Administrator@SC-201511202121>
2016-11-04 22:13:12 +0800

    分拣站model

---
+++ logisticsSystem/src/main/java/com/zhuoyue/model/SortStation.java
@@ -0,0 +1,48 @@
+package com.zhuoyue.model;
+/*
+ * @author 吴兵兵
+ * */
+public class SortStation {
+	private int id;
+	private int station_id;
+	private int rank;
+	private int province;
+	private int city;
+	private int area;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getStation_id() {
+		return station_id;
+	}
+	public void setStation_id(int station_id) {
+		this.station_id = station_id;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public int getProvince() {
+		return province;
+	}
+	public void setProvince(int province) {
+		this.province = province;
+	}
+	public int getCity() {
+		return city;
+	}
+	public void setCity(int city) {
+		this.city = city;
+	}
+	public int getArea() {
+		return area;
+	}
+	public void setArea(int area) {
+		this.area = area;
+	}
+}
